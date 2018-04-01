a930fbf73fadee6e85b7c67563b8647ae103a78d
Reggie Romano <romanoreggie@gmail.com>
2018-04-01 18:09:47 -0500

    project complete

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 console.log("up and running");
+
+$(document).ready(
+    function(){
+        $('#button').click(
+            function(){
+                var toAdd = $('input[name=ListItem]').val();
+                 $('ol').append('<li>' + toAdd + '</li>');
+            });
+
+      $(document).on('dblclick','li', function(){
+        $(this).toggleClass('strike').fadeOut('slow');
+      });
+
+      $('input').focus(function() {
+        $(this).val('');
+      });
+
+      $('ol').sortable();
+
+    }
+);
